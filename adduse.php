1578d10815b694cfc0ff3457c683f17157c4f4aa
nguyenduymanh <mrmanhkut3@gmail.com>
2017-03-23 22:47:30 +0700

    addus

--- adduse.php
+++ adduse.php
@@ -3,5 +3,38 @@
  * Created by PhpStorm.
  * User: MrManh Godlike
  * Date: 23/03/2017
- * Time: 15:51
+ * Time: 15:13
  */
+
+
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="form-group">
+            <form action="user.php" method="get">
+                <div class="form-group">
+                    <label for="Fullname">Fullname</label>
+                    <input name="Fullname" type="text" class="form-control" id="Fullname" placeholder="Fullname">
+                    </input>
+                </div>
+                <div class="form-group">
+                    <label for="Emai">Email</label>
+                    <input name="Email" type="EMAIL" class="form-control" id="Email" placeholder="Email">
+                    </input>
+                </div>
+                <div class="form-group">
+                    <label for="Birthday">Brithday</label>
+                    <input name="Birthday" type="date" class="form-control" id="Birthday" placeholder="Brithday">
+                    </input>
+                </div>
+                <button class="form-group" type="submit">OKay</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
